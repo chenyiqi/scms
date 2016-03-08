2e08c65db95ed506cf94323efc7e645e09931d9c
chenyiqi <363175473@qq.com>
2016-03-08 23:49:28 +0800

    add config

---
+++ config/express.js
@@ -0,0 +1,11 @@
+var express = require('express'),
+	bodyParser = require('body-parser');
+
+
+module.exports = function() {
+	console.log('init express...');
+	var app = express();
+
+
+	return app;
+};
